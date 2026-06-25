122bea86fe0d24943f1f06ee669b30650e57c68a
raoz95447-oss <raoz95447@gmail.com>
2026-06-26 00:05:56 +0500

    Initialize Flutter app with basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,3 @@
-
+import 'package:flutter/material.dart';
+void main(){runApp(MyApp());}
+class MyApp extends StatelessWidget{const MyApp({super.key});@override Widget build(BuildContext context){return MaterialApp(title:'Rizq Tok',home:Scaffold(appBar:AppBar(title:Text('Rizq Tok')),body:Center(child:Text('Mubarak ho bhai! APK ban rahi hai!',style:TextStyle(fontSize:20)))));}}
